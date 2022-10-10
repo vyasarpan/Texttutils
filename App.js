e8ad5916dd71bb885b44dde8089e7d5e2e149564
Arpan Vyas <30585797+vyasarpan@users.noreply.github.com>
2022-10-10 11:51:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type, 
+    })
+    setTimeout(() => {
+      setAlert(null)
+    },3000)
+  };
+  
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+  return (
+    <>
+      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+        <Alert alert={alert} />
+        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" />
+      </div>
+    </>
+  );
+}
+
+export default App;
